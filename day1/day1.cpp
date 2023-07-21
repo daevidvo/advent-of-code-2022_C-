1be711d0d82174f001872599346ab5250a25b726
daevidvo <itsdavidvo@gmail.com>
2023-07-20 18:05:03 -0700

    finished day 1 part 2 code

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -24,7 +24,44 @@ void findMax(std::vector<std::vector<std::string>> const& v) {
 		sum = 0;
 	}
 
-	std::cout << max << std::endl;
+	std::cout << "The highest amount of calories carried by an elf is: " << max << std::endl;
+}
+
+void findTopThreeMax(std::vector<std::vector<std::string>> const& v) {
+	// YIKES THIS IS MESSY LMAO
+	std::vector<int> maxes;
+
+	for (int x = 0; x < v.size(); x += 1) {
+		int sum = 0;
+
+		for (int y = 0; y < v.at(x).size(); y += 1) {
+			int num = std::stoi(v.at(x).at(y));
+
+			sum += num;
+		}
+		
+		maxes.push_back(sum);
+		sum = 0;
+	}
+
+	// bubble sort
+	for (int x = 0; x < maxes.size(); x += 1) {
+		for (int y = 0; y < maxes.size(); y += 1) {
+			if (maxes.at(y) > maxes.at(x)) {
+				int temp = maxes.at(x);
+				maxes.at(x) = maxes.at(y);
+				maxes.at(y) = temp;
+			}
+		}
+	}
+
+	int sumMaxes = 0;
+
+	for (int x = 0; x < 3; x += 1) {
+		sumMaxes += maxes.at(maxes.size() - x - 1);
+	}
+
+	std::cout << "The total calories of the top three elves are: " << sumMaxes << std::endl;
 }
 
 bool stringToBool(std::string const& s) {
@@ -54,8 +91,18 @@ int main() {
 			dataVector.push_back(dataString);
 		}
 	}
-	
+
+	/*
+	================================ PART 1 ================================ 
+	*/
+
 	findMax(v);
+
+	/*
+	================================ PART 2 ================================ 
+	*/
+
+	findTopThreeMax(v);
 
 	return 0;
 }
