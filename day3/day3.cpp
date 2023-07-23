0c97943696d4ca30bfa9dc8567300058dd121d25
daevidvo <itsdavidvo@gmail.com>
2023-07-22 22:43:24 -0700

    setting up day3

---
+++ day3/day3.cpp
@@ -0,0 +1,27 @@
+// AoC 2022 day 3: RuckSack Reorganization
+// https://adventofcode.com/2022/day/3
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	/* === PART 1 === */
+	
+	/*
+	TODO: Find the sum of the properties of the duplicate item types in each rucksack
+
+	Setup Parts;
+	- Retrieve the data from data.txt
+	- Hashmap for property values (a-z = 1-26 and A-Z =	27-52
+	- Create an int variable for the sum of property values
+
+	Approach:
+	- Split the rucksacks into two halves (based on the length of the string)
+	- Find the duplicate letter (case sensitive) in both halves
+	- Find the value of the duplicate letter through the property value hashmap
+	- sum+=value
+	- std::cout << sum << std::endl;
+	*/
+	return 0;
+}
